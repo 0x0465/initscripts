e8265a76ef60563a73d0b90548bb1a88c162b901
dr6g1 <35386414+dr6g1@users.noreply.github.com>
2021-08-31 10:21:29 +0200

    Update initscript.sh

--- initscript.sh
+++ initscript.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 dir=/home/"$(whoami)"
-read -p  "Będziem robić init, gotowy? [y/N]" -n 1 -r
+read -p  "Are you sure? [y/N]" -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
         exit 1
         fi
+        
+# Ignore case-sensitive in autocompletion       
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+echo 'set completion-ignore-case On' >> ~/.inputrc     
+
 cd "$dir" || exit
 if [ -d "$dir"/dotfiles ];then
-    read -p "Katalog już istnieje, czy chcesz usunąć i pobrać od nowa? [y/N]" -n 1 -r
+    read -p "Directory ~/dotfiles already exists, overwrite? [y/N]" -n 1 -r
     echo
     if [[ ! $REPLY =~ ^^[Yy]$ ]]
         then
-        echo "Nic nie robię"
+        echo "Doing nothing, script aborted"
         exit 1
     else
-        echo "Usuwam dotfiles"
+        echo "Deleting ~/dotflies"
         rm -rf "$dir"/dotfiles
         rm "$dir"/.vimrc
         rm "$dir"/.tmux.conf
-        echo "Ściągam z gita"
+        echo "Downloading files from github"
         git clone https://github.com/dr6g1/dotfiles
         find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
     fi
 else
      rm "$dir"/.vimrc
      rm "$dir"/.tmux.conf
-     echo "Ściągam z gita"
+     echo "Downloading files from github"
      git clone https://github.com/dr6g1/dotfiles
      find "$dir"/dotfiles -maxdepth 1 -type f \( -name ".*" ! -name ".git" \) | while read file;do ln -s "$file" "$dir"/;done
-echo "Skrypt gotowy"
+echo "Script ready"
 fi
